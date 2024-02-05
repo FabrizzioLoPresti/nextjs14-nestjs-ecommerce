0c6bcf87e6da05676606d8edfd5eb8fa044e8e29
FabrizzioLoPresti <fabrizziolopresti1999@gmail.com>
2024-02-05 10:07:41 -0300

    Server Slider Query Fix

--- frontend/src/components/home/products/slider-server.tsx
+++ frontend/src/components/home/products/slider-server.tsx
@@ -4,7 +4,10 @@ import Slider from './slider';
 type Props = {};
 
 const SliderServer = async (props: Props) => {
-  const products = await fetchProducts(1, 10);
+  const products = await fetchProducts(1, 10, {
+    category: undefined,
+    sort: undefined,
+  });
 
   return <Slider products={products} />;
 };
